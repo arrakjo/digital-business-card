0e1c802b653b07b2da29d422c63bd11c6960df69
Joosep Arrak <jooseparrak@gmail.com>
2022-02-06 19:21:10 +0200

    Added CTA button functionality

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./Info.css";
 
+function emailButton() {
+  window.location.href = "mailto:jooseparrak@gmail.com";
+}
+
+function linkedinButton() {
+  window.location.href = "https://www.linkedin.com/in/jooseparrak/";
+}
+
 function Info() {
   return (
     <div className="card-info">
@@ -22,7 +30,7 @@ function Info() {
           jooseparrak.eu
         </a>
         <div className="cta">
-          <button className="btn">
+          <button className="btn" onClick={emailButton}>
             <img
               src={require("../img/email.png")}
               alt="Email"
@@ -30,7 +38,7 @@ function Info() {
             />
             Email
           </button>
-          <button className="btn-alt">
+          <button className="btn-alt" onClick={linkedinButton}>
             <img
               src={require("../img/linkedin-alt.png")}
               alt="Email"
